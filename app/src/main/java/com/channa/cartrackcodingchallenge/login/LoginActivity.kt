ecad1d78099b77c4ad3d55e1db7da1eb5f67d348
Channa Edirisinghe <channa.lakruwan@gmail.com>
2019-03-28 00:23:13 +0800

    - implement login screen flag support

--- app/src/main/java/com/channa/cartrackcodingchallenge/login/LoginActivity.kt
+++ app/src/main/java/com/channa/cartrackcodingchallenge/login/LoginActivity.kt
@@ -2,32 +2,39 @@ package com.channa.cartrackcodingchallenge.login
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.channa.cartrackcodingchallenge.R
 import com.mukesh.countrypicker.CountryPicker
 import com.mukesh.countrypicker.OnCountryPickerListener
 
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var flagContainerLinearLayout: LinearLayout
     lateinit var countryTextView: TextView
+    lateinit var countryFlagImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.channa.cartrackcodingchallenge.R.layout.activity_login)
 
-        countryTextView = findViewById(com.channa.cartrackcodingchallenge.R.id.tv_country)
-        countryTextView.setOnClickListener(View.OnClickListener {
+        flagContainerLinearLayout = findViewById(R.id.ll_flag_container)
+        countryTextView = findViewById(R.id.tv_country)
+        countryFlagImageView = findViewById(R.id.iv_flag)
+
+        flagContainerLinearLayout.setOnClickListener(View.OnClickListener {
 
             val builder = CountryPicker.Builder().with(this)
                     .listener(OnCountryPickerListener { country ->
                         run {
-                            Toast.makeText(this, country.name, Toast.LENGTH_LONG).show()
                             countryTextView.text = country.name
-
+                            countryFlagImageView.setImageResource(country.flag)
                         }
                     }).build().showDialog(this)
         })
     }
+
 }
