f335c6fa485e87ce6a9227e26b09705d885c915f
Channa Edirisinghe <channa.lakruwan@gmail.com>
2019-03-28 19:27:48 +0800

    - implemented logic for phone number split (split number and extension) - all tests passed

--- app/src/main/java/com/channa/cartrackcodingchallenge/data/source/remote/response/UserResponse.kt
+++ app/src/main/java/com/channa/cartrackcodingchallenge/data/source/remote/response/UserResponse.kt
@@ -12,9 +12,25 @@ class UserResponse(
 ) {
 
     val getPhoneNumber: String
-        get() = ""
+        get() {
+            var phoneList = phone.split(" ")
+            var phoneNumber = phoneList.getOrNull(0)
+
+            if (phoneNumber == null || phoneNumber.isBlank()) phoneNumber = "N/A"
+
+            return phoneNumber
+
+        }
 
     val getPhoneNumberExtension: String
-        get() = ""
+        get() {
+            var phoneList = phone.split(" ")
+            var extensionNumber = phoneList.getOrNull(1)
+
+            if (extensionNumber == null || extensionNumber.isBlank()) extensionNumber = "N/A"
+
+            return extensionNumber
+
+        }
 
 }
